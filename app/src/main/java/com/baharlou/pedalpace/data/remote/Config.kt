b19596c61aced7b7d2ee56ae92b6db12aa48c5d9
mona-baharlou <mona.baharlou755@gmail.com>
2025-12-24 14:44:59 +0330

    Config updated

--- app/src/main/java/com/baharlou/pedalpace/data/remote/Config.kt
+++ app/src/main/java/com/baharlou/pedalpace/data/remote/Config.kt
@@ -3,7 +3,7 @@ package com.baharlou.pedalpace.data.remote
 import com.baharlou.pedalpace.BuildConfig
 
 object Config {
-    const val API_KEY ="e4ee7a511491b4d4cc201c0510057da5"
+    const val API_KEY = BuildConfig.WEATHER_API_KEY
 
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val ICON_URL = "https://openweathermap.org/img/wn/"
